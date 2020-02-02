799f0ca30a7c179a9c65eece8c4d9519433df531
Dsenzano <DanielSenzano@gmail.com>
2020-02-02 11:50:17 -0500

    BookPage 2nd commit

--- src/test/java/pages/BookPage.java
+++ src/test/java/pages/BookPage.java
@@ -15,10 +15,10 @@ public class BookPage {
     @FindBy (xpath="//input")
     public WebElement location;
 
-    @FindBy (xpath="//input[@name='checkin']")
+    @FindBy (xpath="//*[@id=\"FMP-target\"]/div/div[1]/div/div/div/div/div/div[2]/div/form/div[2]/div/div/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[2]/td[2]")
     public WebElement checkIn;
 
-    @FindBy (xpath="//input[@name='checkout']")
+    @FindBy (xpath="//*[@id=\"FMP-target\"]/div/div[1]/div/div/div/div/div/div[2]/div/form/div[2]/div/div/div/div[3]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[3]/td[2]")
     public WebElement checkOut;
 
     @FindBy (xpath = "//button[@class='_7ykwo4']")
--- src/test/java/step_definitions/BookPage_STEPS.java
+++ src/test/java/step_definitions/BookPage_STEPS.java
@@ -5,17 +5,18 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import pages.BookPage;
 import pages.HomePage;
+import step_definitions.step_impl.BookPage_impl;
 import step_definitions.step_impl.HomePage_impl;
 import util.Driver;
 
 public class BookPage_STEPS {
     private static WebDriver driver = Driver.getDriver();
-    BookPage homePage = new BookPage();
-    HomePage_impl homePage_impl = new HomePage_impl();
+    BookPage bookPage = new BookPage();
+    BookPage_impl bookPage_impl = new BookPage_impl();
 
     @When("User enters {string}, checks in today, checks out in a week, and enters {string} guests")
-    public void user_enters_checks_in_checks_out_in_and_enters_guests(String location, String guests) {
-
+    public void user_enters_checks_in_today_checks_out_in_week_and_enters_guests(String location, String guests) {
+        bookPage_impl.bookNow(location,guests);
     }
 
     @Then("Verify search parameters are correct")
